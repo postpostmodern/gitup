098b8f963f640b8b54825fb2398496f52d85072a
Jason T Johnson <design@postpostmodern.com>
2009-02-01 00:12:59 -0600

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,64 @@
+# Gitup!
+
+__Scenario:__ You are working on a simple web site. You make some changes to a bunch of files. You need to upload the changed files to the server. Who knows what files you've modified in the past few days? Well, git knows. Run `gitup --since="last wednesday"`, and you're done.
+
+# Installation
+
+Put gitup.rb somewhere and alias it as `gitup`.
+
+# Requirements
+
+Gitup is a Ruby script. It is intended to work on __OS X in conjunction with Git and Transmit__ using Transmit's _Dock Send_ feature. You need Ruby installed to run it. If you don't use a Mac, Git and Transmit, Gitup probably won't do you much good.
+
+The one exception is if you have an app to which you want to send files other than Transmit. It can do that. You just have to specify the app.
+
+# Usage
+
+If you run gitup -h, you'll see this:
+
+    Usage: gitup.rb [-s|--skip-preview] [--application=<app_name>] <git-log options>
+    Calling gitup.rb without arguments will build the file list from the most recent commit.
+    See Commit Limiting in git-log help for options on specifying the commits.
+    
+    Other options:
+      --application=<app_name>   Specify an application other than Transmit.
+      -h, --help                 Show this message.
+      -s, --skip-preview         Send files straight to Transmit without a prompt.
+        
+That pretty much explains it. Run `git help log` to see how to specify exactly which commits you want to use.
+
+# Examples
+
+Send files modified in the last commit to Transmit (Gitup will let you know how many files will be sent and offer you the option to view a list of the files or abort.):
+
+    gitup
+
+Send files modified in the last 3 commits to Transmit:
+
+    gitup -3
+
+Send files modified in the last 3 commits to Transmit immediately (no prompting or anything):
+
+    gitup -s -3
+    
+Send files modified since yesterday to Transmit:
+
+    gitup --since=yesterday
+    
+Send files modified since Monday to Transmit (quotes are required):
+
+    gitup --since="last monday"
+    
+Send files since the specified commit to Transmit:
+
+    gitup dcd2c68..
+    
+Send files between the two specified commits to Textmate:
+
+    gitup --application=Textmate dcd2c68..bf75dd6
+    
+# BETA WARNING
+
+___This script was hacked together in a short time to scratch an itch. It works for me (using Git 1.6, Leopard, and Transmit), but systems vary greatly. USE GITUP AT YOUR OWN RISK!! I AM NOT RESPONSIBLE FOR DAMAGE CAUSED BY THIS SCRIPT.___
+
+You may very well know more about Ruby and Git than I do. If you find any flaws with this code or have any recommendations as to how it can be improved, please fork it and let me know.
---
+++ gitup.rb
@@ -0,0 +1,132 @@
+#!/usr/bin/env ruby
+
+class Gitup
+  DEFAULT_APPLICATION = 'Transmit'
+  
+  def initialize(argv)
+    # Set default application
+    @application = DEFAULT_APPLICATION
+    # Parse ARGV for application option.
+    # This is awkward, but I'm too lazy to learn how to use OptionParser.
+    argv.join('|').scan(/--application=[\w\s]+/) do |app|
+      @application = app.match(/[\w\s]+$/)[0]
+      argv.delete app
+    end
+    # This option is a bit easier to find.
+    @skip_preview = argv.delete('-s') || argv.delete('--skip-preview')
+    # Do the help thing.
+    if argv.include?('-h') || argv.include?('--help')
+      @help = true
+    elsif argv.empty?
+      # This just asks git-log for the last commit
+      @options = '-1'
+    else
+      # If any other options/arguments are given, just pass them on to git-log
+      @options = []
+      argv.each do |arg|
+        # Make sure options with a space are quoted
+        @options<< arg.gsub(/=((.*\s)+.*)$/, '="\1"')
+      end
+    end
+  end
+
+  # Do the thing
+  def process!
+    if @help
+      help
+    elsif commits.empty?
+      puts "No files found."
+    else
+      if @skip_preview
+        upload_files
+      else
+        prompt
+      end
+    end
+  end
+
+  # Get the file list from git-log
+  def git_log
+    @git_log ||= `git log --name-status --relative --pretty=format:">>> %h %s" #{@options}`
+  end
+
+  # Get groups of files (grouped by commit)
+  def commits
+    @commits ||= git_log.split(/^\n?>>> /).collect do |group|
+      files = group.split("\n")
+      # The name is the commit message
+      name = files.shift
+      # What's left is the list of files
+      files.collect! do |file|
+        # Collect the ones that were added or modified
+        file.sub(/[AM]\t/, '') if file =~ /[AM]\t/
+      end
+      # Get rid of the nils
+      files.compact!
+      # We don't want any files that have been deleted
+      files.delete_if { |file| !File.exists?(file) }
+      { :name => name, :files => files } unless name.nil? || files.nil? || files.empty?
+    end.compact 
+  end
+
+  # Get the entire list of files
+  def file_list
+    files = commits.collect { |commit| commit[:files] }.flatten
+  end
+
+  # Displays help info
+  def help
+    puts "
+      Usage: gitup.rb [-s|--skip-preview] [--application=<app_name>] <git-log options>
+      Calling gitup.rb without arguments will build the file list from the most recent commit.
+      See Commit Limiting in git-log help for options on specifying the commits.
+
+      Other options:
+        --application=<app_name>   Specify an application other than Transmit.
+        -h, --help                 Show this message.
+        -s, --skip-preview         Send files straight to Transmit without a prompt.
+      "
+  end
+
+  # Lists commits and prompts for action
+  def prompt
+    # List commits and file counts
+    commits.each do |commit|
+      puts commit[:files].size.to_s.rjust(6) + ' Files:' + commit[:name]
+    end
+    # Prompt for action
+    print "Continue/List Files/Abort [Cla] "
+    input = STDIN.gets.chomp.strip.downcase
+    # Respond accordingly
+    case input
+    when 'l'
+      list_files
+    when 'c'
+      upload_files
+    else
+      puts "  Aborted"
+      exit
+    end
+  end
+
+  # Sends files to Transmit
+  def upload_files
+    puts "Files sent to #{@application}!"
+    puts `open -ga '#{@application}' #{file_list.join(' ')}`
+  end
+
+  # Displays the complete file list
+  def list_files
+    commits.each do |commit|
+      puts "========================================================="
+      puts "Files: #{commit[:name]}"
+      puts "---------------------------------------------------------"
+      puts commit[:files].join("\n")
+    end
+    puts "========================================================="
+    prompt
+  end
+
+end
+
+Gitup.new(ARGV).process!
